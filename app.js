b9ead8d956e5572ddcfa415929bbca7b45462185
underdog9090 <erikhakizimana03@gmail.com>
2024-04-05 21:01:23 +0200

    added backend

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+const cors = require("cors")
 const express = require("express");
 const app = express();
+const productRouter = require("./src/routes/product.routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/intro/product", productRouter )
+app.use(cors("*"))
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Connected to db"
+    })
+})
+
 module.exports = app;
