afa202892d3da8c7ab90655f71650dd56b4aa704
underdog9090 <erikhakizimana03@gmail.com>
2024-04-05 16:19:51 +0200

    added atlas connection

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/myapp");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
