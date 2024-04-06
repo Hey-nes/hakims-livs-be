18f3e6ec6aee063b3e4287f822b020f801c58b15
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-04-06 20:35:40 +0200

    fixed imports

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cors = require("cors")
 const express = require("express");
 const app = express();
-const productRouter = require("./src/routes/product.routes");
+const productRouter = require("./src/routes/productRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-const productModel = require("../models/product.model");
+const productModel = require("../models/productModel");
 
 const getProducts = async (req, res) => {
   try {
