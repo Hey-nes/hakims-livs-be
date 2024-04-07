51acbdcad8ffb1484bf69ce28a86d1e295d1828f
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-04-07 17:00:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", productRouter);
+app.use("/", productRouter);
 
 app.get("/", (req, res) => {
   res.json({
