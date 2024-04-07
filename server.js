e1aa38874591bcfa873e70a3e358eb3347e7ff3f
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-04-07 16:08:31 +0200

    reverted changes

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const { mongoose, connectToDatabase } = require("./src/config/database");
-const productController = require("./src/controllers/productController");
-const app = express();
+const app = require("./app");
 
 connectToDatabase();
 
+// Added CORS middleware
 app.use(cors());
-
-app.get("/api/intro/product", productController.getProducts);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
