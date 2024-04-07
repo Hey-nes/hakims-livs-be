5780afc111130788cff62c0981a9b959da5eeacb
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-04-07 15:49:11 +0200

    added productController for route handling

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const { mongoose, connectToDatabase } = require("./src/config/database");
+const productController = require("./src/controllers/productController");
 const app = express();
 
 connectToDatabase();
 
 app.use(cors());
 
-app.get("/api/intro/product", (req, res) => {
-  res.json({ message: "Products fetched successfully" });
-});
+app.get("/api/intro/product", productController.getProducts);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
