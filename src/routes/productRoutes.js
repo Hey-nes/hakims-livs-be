8d46a7fbd0792ad434587431c4a7726a2f7e7901
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-04-07 10:40:30 +0200

    fixed routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -6,7 +6,7 @@ const {
   getProducts,
 } = require("../controllers/productController");
 
-router.get("/", getProducts);
-router.post("/create", createNewProduct);
+router.get("/api/intro/product", getProducts);
+router.post("/api/intro/product/create", createNewProduct);
 
 module.exports = router;
