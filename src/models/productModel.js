f48cf1cf3348457b9ed8937cc42df41f1405c87d
underdog9090 <erikhakizimana03@gmail.com>
2024-04-15 21:54:36 +0200

    uppdated product model to products in Frontend

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -6,6 +6,10 @@ const productSchema = mongoose.Schema({
   description: { type: String, required: true },
   category: { type:mongoose.Schema.Types.ObjectId, ref:"Category" },
   price: { type: Number, required: true },
+  manufacturer: {type: String, required: true},
+  weight: { type: Number, required: true },
+  bestBefore: { type: Date, default: new Date("2027-01-31") },
+  stock: { type: Number, required: true },
 });
 
 const productModel = mongoose.model("products", productSchema);
