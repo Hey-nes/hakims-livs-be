6d3b04d97c99dcb66c74a39102dc1a3f54db9ed2
underdog9090 <erikhakizimana03@gmail.com>
2024-04-15 02:38:36 +0200

    added cred on category

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -4,5 +4,6 @@ const routController = require("../controllers/categoryController");
 
 router.get("/api/intro/category", routController.getCategory);
 router.post("/api/intro/category/create", routController.createNewCategory);
-
+router.put("/api/intro/category/:id", routController.updateCategory);
+router.delete("/api/intro/category/:id", routController.deleteCategory);
 module.exports = router;
