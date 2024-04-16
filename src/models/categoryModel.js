782dc2c934b6e5eb55acd99893446e4be41694d6
underdog9090 <erikhakizimana03@gmail.com>
2024-04-16 09:33:47 +0200

    added required

--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique:true },
   },
 
 );
---
+++ src/utils/categorySeeder.js
@@ -0,0 +1,22 @@
+// const Category = require("../models/Category");
+
+// const defaultCategories = [
+//   { name: "Mejeri" },
+//   { name: "Frukt & Grönt" },
+//   { name: "Bröd & Bakverk" },
+//   { name: "Fisk & Skaldjur" },
+//   { name: "Snacks & Godis" },
+//   { name: "Skafferi" },
+//   { name: "Drycker" },
+// ];
+
+// async function seedCategories() {
+//   try {
+//     await Category.insertMany(defaultCategories);
+//     console.log("Default categories inserted successfully.");
+//   } catch (error) {
+//     console.error("Error seeding categories:", error);
+//   }
+// }
+
+// module.exports = seedCategories;
