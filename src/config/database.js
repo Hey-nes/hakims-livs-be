5be3d59f002b48591b10cacadbc1ed299916bb6b
underdog9090 <erikhakizimana03@gmail.com>
2024-04-17 14:48:36 +0200

    tetesf filtering

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose");
 
-async function connectToDatabase() {
-  try {
-    await mongoose.connect(process.env.MONGOOSE_LIVE_URI);
-    console.log("Connected to MongoDB successfully!");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-}
-
 // async function connectToDatabase() {
 //   try {
-//     await mongoose.connect(process.env.DATABASE_PATH);
+//     await mongoose.connect(process.env.MONGOOSE_LIVE_URI);
 //     console.log("Connected to MongoDB successfully!");
 //   } catch (error) {
 //     console.error("Error connecting to MongoDB:", error);
 //   }
 // }
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.DATABASE_PATH);
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
 module.exports = { mongoose, connectToDatabase };
 // i used localy MONGOOSE_LIVE_URI
