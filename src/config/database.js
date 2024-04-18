86e3e687da058a93f2c58ac3fbd4af5cbf4effe1
underdog9090 <erikhakizimana03@gmail.com>
2024-04-18 14:53:51 +0200

    CHNGED ENVIROMENT VARIABLES

--- src/config/database.js
+++ src/config/database.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.DATABASE_PATH);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
