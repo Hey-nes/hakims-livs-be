f728a165828d889d85f586a686f7850a299c1dde
underdog9090 <erikhakizimana03@gmail.com>
2024-04-29 12:00:25 +0200

    adjusted validering and getAllCustomers

--- src/controllers/customerControllers.js
+++ src/controllers/customerControllers.js
@@ -40,6 +40,20 @@ const getCustomer = async (req, res) => {
   }
 };
 
+
+const getAllCustomers = async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.status(200).json(customers);
+  } catch (error) {
+    console.error("Error fetching customers:", error);
+    res
+      .status(500)
+      .json({ message: "Failed to fetch customers", error: error.message });
+  }
+};
+
+
 const updateCustomer = async (req, res) => {
   try {
     const customerId = req.params.customerId;
@@ -75,6 +89,7 @@ const deleteCustomer = async (req, res) => {
 module.exports = {
   createCustomer,
   getCustomer,
+  getAllCustomers,
   updateCustomer,
   deleteCustomer,
 };
--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {validateCreateCustomer} = require("../middlewares/customerValidation"); 
-const { createCustomer, getCustomer, updateCustomer, deleteCustomer } = require("../controllers/customerControllers");
+const { createCustomer, getCustomer, getAllCustomers,updateCustomer, deleteCustomer } = require("../controllers/customerControllers");
 
 router.post('/create', validateCreateCustomer, createCustomer); 
 router.get('/:customerId', getCustomer); 
+router.get('/', getAllCustomers);
 router.put('/:customerId', validateCreateCustomer, updateCustomer);
 router.delete('/:customerId', deleteCustomer); 
 
